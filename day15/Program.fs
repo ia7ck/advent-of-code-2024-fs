be917d43ba666e95384200e63edefc4cef3a9262
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-12-16 00:16:47 +0900

    再帰的に box を移動

--- day15/Program.fs
+++ day15/Program.fs
@@ -13,7 +13,9 @@ type Dir =
     | D
     | R
 
-let reverse (a: 'T[][]) = Array.map (Array.rev) a
+let swap (i, j) (a: 'T[]) =
+    let x, y = a[i], a[j]
+    a |> Array.updateAt i y |> Array.updateAt j x
 
 let transpose (a: 'T[][]) =
     let h, w = a.Length, a[0].Length
@@ -23,42 +25,51 @@ let findRobot (map: Cell[][]) =
     List.allPairs [ 0 .. (map.Length - 1) ] [ 0 .. (map[0].Length - 1) ]
     |> List.find (fun (i, j) -> map[i][j] = Robot)
 
-let moveLeft (map: Cell[][]) =
+let rec pushLeft (i, j) (map: Cell[][]) =
+    assert (map[i][j] = Box)
+
+    match map[i][j - 1] with
+    | Wall -> None
+    | Empty ->
+        // #..OOO. -> # .O.OO.
+        let newRow = map[i] |> swap (j - 1, j)
+        map |> Array.updateAt i newRow |> Some
+    | Box ->
+        pushLeft (i, j - 1) map
+        |> Option.map (fun map ->
+            assert (map[i][j - 1] = Empty)
+            // delegate
+            pushLeft (i, j) map |> Option.get)
+    | Robot -> failwith "!?"
+
+let rec moveLeft (map: Cell[][]) =
     let ri, rj = findRobot map
 
     match map[ri][rj - 1] with
-    | Wall -> Some map
+    | Wall -> map
     | Empty ->
-        let newRow = map[ri] |> Array.updateAt (rj - 1) Robot |> Array.updateAt rj Empty
-        map |> Array.updateAt ri newRow |> Some
+        let newRow = map[ri] |> swap (rj - 1, rj)
+        map |> Array.updateAt ri newRow
     | Box ->
-        let start = [ 0 .. (rj - 1) ] |> List.findBack (fun j -> map[ri][j] <> Box)
-
-        if map[ri][start] <> Empty then
-            None
-        else
-            // #....OOO@..
-            // ↓
-            // #...OOO@...
-            let newRow =
-                map[ri]
-                |> Array.mapi (fun j' c ->
-                    if start <= j' && j' < rj - 1 then Box
-                    else if j' = rj - 1 then Robot
-                    else if j' = rj then Empty
-                    else c)
-
-            map |> Array.updateAt ri newRow |> Some
+        map
+        |> pushLeft (ri, rj - 1)
+        |> Option.map (fun map ->
+            assert (map[ri][rj - 1] = Empty)
+            // delegate
+            moveLeft map)
+        |> Option.defaultWith (fun () -> map)
     | Robot -> failwith "!?"
 
 let moveRight (map: Cell[][]) =
-    map |> reverse |> moveLeft |> Option.map reverse
+    let reverse (a: 'T[][]) = Array.map (Array.rev) a
+
+    map |> reverse |> moveLeft |> reverse
 
 let moveUp (map: Cell[][]) =
-    map |> transpose |> moveLeft |> Option.map transpose
+    map |> transpose |> moveLeft |> transpose
 
 let moveDown (map: Cell[][]) =
-    map |> transpose |> moveRight |> Option.map transpose
+    map |> transpose |> moveRight |> transpose
 
 let part1 ((map, moves): (Cell[][] * Dir seq)) =
     let map =
@@ -71,7 +82,7 @@ let part1 ((map, moves): (Cell[][] * Dir seq)) =
                 | D -> moveDown
                 | R -> moveRight
 
-            mv map |> Option.defaultWith (fun () -> map))
+            mv map)
 
     List.allPairs [ 0 .. (map.Length - 1) ] [ 0 .. (map[0].Length - 1) ]
     |> List.sumBy (fun (i, j) -> if map[i][j] = Box then 100 * i + j else 0)
@@ -111,11 +122,11 @@ module Example =
     type MoveTest() =
         [<Fact>]
         let moveLeftTest () =
-            parseMap "#..OO@." |> moveLeft |> should equal (Some(parseMap "#.OO@.."))
+            parseMap "#..OO@." |> moveLeft |> should equal (parseMap "#.OO@..")
 
         [<Fact>]
         let moveRightTest () =
-            parseMap "#.@OO.." |> moveRight |> should equal (Some(parseMap "#..@OO."))
+            parseMap "#.@OO.." |> moveRight |> should equal (parseMap "#..@OO.")
 
 
         [<Fact>]
@@ -123,14 +134,14 @@ module Example =
             parseMap "#..OO@."
             |> transpose
             |> moveUp
-            |> should equal (Some(parseMap "#.OO@.." |> transpose))
+            |> should equal (parseMap "#.OO@.." |> transpose)
 
         [<Fact>]
         let moveUpTest () =
             parseMap "#.@OO.."
             |> transpose
             |> moveDown
-            |> should equal (Some(parseMap "#..@OO." |> transpose))
+            |> should equal (parseMap "#..@OO." |> transpose)
 
     let input =
         "########
